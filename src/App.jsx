4fe8e5a70b43af3f99777fe0df178912fe813002
Amlana Kumar Sahoo <amlana.ksahoo@gmail.com>
2026-01-13 20:33:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Header from "./components/Header";
+import ClassHeader from "./components/classHeader";
+
+function App() {
+   // return <ClassHeader />  // Uncomment this line to use the class-based component(Old Approch)
+    return <Header /> // Functional Component (New Approch)
+}
+export default App
---
+++ src/components/ClassHeader.jsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBasket, faTags } from "@fortawesome/free-solid-svg-icons";
+
+class ClassHeader extends Component {
+    render() {
+        return (
+            <header className="header">
+                <div className="container">
+                    <a href="/" className="link"></a>
+                    <FontAwesomeIcon icon={faTags} className="fa-icon" />
+                    <span className="brand-title">Eazy Stickers</span>
+                    <nav className="nav">
+                    <ul>
+                        <li >
+                            <a href="/home" className="nav-link">Home</a>
+                        </li>
+                        <li >
+                            <a href="/about" className="nav-link">About</a>
+                        </li>
+                        <li >
+                            <a href="/contact" className="nav-link">Contact</a>
+                        </li>
+                        <li >
+                            <a href="/login" className="nav-link">Login</a>
+                        </li>
+                        <li >
+                            <a href="/cart" className="nav-link"></a>
+                             <FontAwesomeIcon icon={faShoppingBasket} className="fa-icon" />
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        )
+    }
+}
+
+export default ClassHeader;
